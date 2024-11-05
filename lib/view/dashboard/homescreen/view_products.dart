5f4922707d1545494adce22b37b66da20b443b3e
Rana-Basit <130330788+Rana-Basit@users.noreply.github.com>
2024-11-05 15:36:20 +0500

    11/5/24

--- lib/view/dashboard/homescreen/view_products.dart
+++ lib/view/dashboard/homescreen/view_products.dart
@@ -71,12 +71,43 @@ class _ProductViewState extends State<ProductView> {
         'totalPrice': price * quantity,
         'points': points,
         'imageUrl': imageUrl,
+      }).then((value) {
+        _showTopSnackBar(context, '$name added to cart with $points points');
+      }).catchError((error) {
+        _showTopSnackBar(context, 'Failed to add $name to cart');
       });
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('$name added to cart with $points points')),
-      );
     }
   }
+
+  void _showTopSnackBar(BuildContext context, String message) {
+    final overlay = Overlay.of(context);
+    final overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: 50.0, // Adjust the top position as needed
+        left: 20.0,
+        right: 20.0,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              message,
+              style: const TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    overlay?.insert(overlayEntry);
+    Future.delayed(const Duration(seconds: 2), () {
+      overlayEntry.remove();
+    });
+  } 
 
   void _filterProducts(String query) {
     setState(() {
